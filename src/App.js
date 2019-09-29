67a5c8a44f5c5a2db067ee89be272ec8ea48079b
Adrian Arcilla <aarcilla@Adrians-MacBook-Pro.local>
2019-09-29 15:17:53 -0700

    creating romannumeralcalculator problem and readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RomanNumeralComponent from './RomanNumeralComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{backgroundColor: "#153D5C"}}>
+      <img alt="No photo description available." 
+          src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/40454445_490869574712396_8741758426033422336_n.png?_nc_cat=109&amp;_nc_oc=AQnvDU1wyVcbcH0ghUmxqaxvN6aBY6Mrj8r0Ofwb4K9ZpduFM_pEMzWCidfdEeaCo2ZGQjcXGdeoHh5tNRmPopxE&amp;_nc_ht=scontent-lax3-2.xx&amp;oh=4f29bcaa2f8f62b369151b65b5845073&amp;oe=5E3A8A56" 
+          style={{width: "200px", height: "200px"}} />
+        <RomanNumeralComponent />
       </header>
     </div>
   );
---
+++ src/RomanNumeralComponent.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+
+export default function RomanNumeralComponent () {
+    const [calculatedAnswer, updateAnswer] = useState("Nulla");
+    const [input, updateInput] = useState("");
+
+
+    const addAndConvertToRomanNumerals = (ints) => {
+        /* Implement me! */
+        return ints;
+    }
+
+    const addNumbers = (inputString) => {
+        const numbersStringArray = inputString.split(",");
+        const numbers = numbersStringArray.map((numberAsString) => { return parseInt(numberAsString, 10) })
+
+        /* numbers is an array of ints. E.g. [1, 2, 3] */
+        const answer = addAndConvertToRomanNumerals(numbers)
+
+        return answer;
+    }
+
+    const handleChange = (event) => {
+        updateInput(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const answer = addNumbers(input);
+        updateAnswer(answer);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label style={{paddingRight: "10px"}}>
+                    <span style={{paddingRight: "10px"}}>Numbers (separated by commas):</span>
+                    <input type="text" name="input-form" onChange={handleChange}/>
+                </label>
+                <input type="submit" value="Add Numbers" />
+            </form>
+            <div>Answer in Roman Numerals: {calculatedAnswer}</div>
+        </div>
+    )
+}
